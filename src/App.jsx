77912393994cf42e4e473fa87e077bab0cc9c76d
Hari Prasad B G <bghariprasad.developer@gmail.com>
2023-09-02 12:35:16 +0530

    buid fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
 import LandingPage from '../src/components/templates/LandingPage';
 import './App.css'
 
 function App() {
   console.log(">>>")
   return (
-    <ChakraProvider>
-      <LandingPage />
-    </ChakraProvider>
+    <LandingPage />
   )
 }
 
--- src/components/atoms/ResumeBtn/index.jsx
+++ src/components/atoms/ResumeBtn/index.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 function ResumeBtn() {
   return (
     <section>
-      <a rel="noreferrer" href={'ExampleDoc'} download="MyExampleDoc" target='_blank'>
+      <a rel="noreferrer" href={'https://drive.google.com/uc?export=download&id=15ELaZkvpbK-q12euz--G4KlyCtK2P6t2'} download="MyExampleDoc">
         <button className="resume-btn">Download Resume</button>
       </a>
     </section>
